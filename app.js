7d2c0e06cd1bddc02606b186a5d9ca848566d789
Strick4888 <benjaminstrick2016@u.northwestern.edu>
2015-04-13 17:22:28 -0500

    added ratings for the sample messages

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ function seed(){
 	var newMessage1 = new messages({
 		text: "I twisted my ankle on my way to Tech today, but class was so interesting that I couldn't remain upset. I got a lot of work done afterward and felt particularly productive.  Then I had sorbet and it was delicious. Great day.",
 		date: d,
-		phone: "9145557111"
+		phone: "9145557111",
+		rating: 2
 	})
 	newMessage1.save(function(err, newMessage){
 		if(err) return err;
@@ -35,7 +36,8 @@ function seed(){
 	var newMessage2 = new messages({
 		text: "It was a rainy day today and I stayed inside working on my art project.  I felt a little lonely but also accomplished.",
 		date: d,
-		phone: "9145557111"
+		phone: "9145557111",
+		rating: 3
 	})
 	newMessage2.save(function(err, newMessage){
 		if(err) return err;
@@ -46,7 +48,8 @@ function seed(){
 	var newMessage3 = new messages({
 		text: "Today I woke up with a vast skin infection.  I laid in bed all day and was unable to do anything else - hopefully this goes away soon.",
 		date: d,
-		phone: "9145557111"
+		phone: "9145557111",
+		rating: 1
 	})
 	newMessage3.save(function(err, newMessage){
 		if(err) return err;
@@ -57,7 +60,8 @@ function seed(){
 	var newMessage4 = new messages({
 		text: "I have been feeling a lot better, and my former skin infection has gone away.  I also found a 20 dollar bill on the floor. Lucky me!",
 		date: d,
-		phone: "9145557123"
+		phone: "9145557123",
+		rating: 4
 	})
 	newMessage4.save(function(err, newMessage){
 		if(err) return err;
@@ -68,7 +72,8 @@ function seed(){
 	var newMessage5 = new messages({
 		text: "My friend Jenning's dog died and I am feeling very sad. I was very attached to that dog even though it wasn't mine.  I also won 57 games of my favorite video game.",
 		date: d,
-		phone: "9145557123"
+		phone: "9145557123",
+		rating: 2
 	})
 	newMessage5.save(function(err, newMessage){
 		if(err) return err;

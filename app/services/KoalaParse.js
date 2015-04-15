c0f3ebc674790933a9e82349d2bd8fb193c049c7
Tae Heon Jeong <taejeong2013@u.northwestern.edu>
2015-04-15 11:56:04 -0500

    Fixed the parser to deal with messages without a hashtag

--- app/services/KoalaParse.js
+++ app/services/KoalaParse.js
@@ -7,18 +7,19 @@ function KoalaParse(str){
 	var messagePart = twoStrings[0];
 	var ratingPart = parseInt(twoStrings[1]);
 
-
 	if (ratingPart<1){
 		ratingPart = 1;
 	}
 	 else if(ratingPart > 5){
 		ratingPart = 5;
 	}
+	else if(isNaN(ratingPart)){
+		ratingPart = 0;
+	}
 	var returnObj = {message: messagePart, rating: ratingPart};
 	
 	return returnObj;
 	
 }
-
 
 module.exports = KoalaParse;
